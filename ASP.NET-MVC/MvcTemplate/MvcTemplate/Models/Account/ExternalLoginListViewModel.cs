3071e877904b971281f634554b9f96b93c28977d
MarianNikolov <marian.nikolov.net@gmail.com>
2017-06-24 13:18:30 +0300

    Added ASP.NET MVC project. Separating the classes for Account and Manage into separate files in folder Model.

---
+++ ASP.NET-MVC/MvcTemplate/MvcTemplate/Models/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MvcTemplate.Models.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ ASP.NET-MVC/MvcTemplate/MvcTemplate/Models/Manage/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MvcTemplate.Models.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
